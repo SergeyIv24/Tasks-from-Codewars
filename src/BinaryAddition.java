3922adf75cfe8a6ef423713066ad9cc4f20ec25c
SegreyIv <Reger21a@yandex.ru>
2023-11-24 17:58:09 +0300

    +1 task

---
+++ src/BinaryAddition.java
@@ -0,0 +1,28 @@
+//https://www.codewars.com/kata/551f37452ff852b7bd000139
+// 7 kye
+
+public class BinaryAddition {
+    public static String binaryAddition(int a, int b){
+        int sum = a + b;
+        String binaryNum = "";
+        int toBinaryNum = sum;
+        String surplus = "";
+        int surplusNum = sum;
+
+        while (toBinaryNum != 1) {
+            surplusNum = toBinaryNum % 2;
+            toBinaryNum = toBinaryNum / 2;
+            surplus = surplus + surplusNum;
+
+        }
+
+        StringBuilder reverseSurplus = new StringBuilder(surplus);
+        reverseSurplus.reverse();
+        String reversSurplusStr = reverseSurplus.toString();
+
+        binaryNum = "" + toBinaryNum;
+        binaryNum = binaryNum + reversSurplusStr;
+        System.out.println(binaryNum);
+        return binaryNum;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -4,13 +4,7 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-        String s1 = "aretheyhere";
-        String s2 = "yestheyarehere";
-        TwotoOne res = new TwotoOne();
-        res.longest(s1, s2);
-        //System.out.println(sol);
-
-
-
+        BinaryAddition res = new BinaryAddition();
+        res.binaryAddition(99, 99);
     }
 }
