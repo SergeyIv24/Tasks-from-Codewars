01744d6bf774ce770ab9b0d9ae6727a8a11f6544
Sergey Ivanov <Reger21a@yandex.ru>
2024-03-05 11:23:00 +0300

    +Задача

---
+++ src/Kye6/FindTheParityOutlier.java
@@ -0,0 +1,18 @@
+//Kye 6
+//https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/java
+
+package Kye6;
+
+import java.util.Arrays;
+
+public class FindTheParityOutlier {
+    public static int find(int[] integers) {
+        int[] numbers = Arrays.stream(integers).filter(n -> n % 2 != 0).toArray();
+        if (numbers.length == 1) {
+            return numbers[0];
+        } else {
+            return Arrays.stream(integers).filter(n -> n % 2 == 0).toArray()[0];
+        }
+    }
+
+}
--- src/main.java
+++ src/main.java
@@ -1,10 +1,15 @@
 import Kye6.BitCounting;
+import Kye6.FindTheParityOutlier;
 import Kye6.StopGninnipSMySdroW;
 import Kye6.SumOfDigitsDigitalRoot;
 
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println(BitCounting.countBits(7));
+        int[] ints = {1, 3, 4, 5, 7, 9};
+        System.out.println(FindTheParityOutlier.find(ints));
+
 
 
 
